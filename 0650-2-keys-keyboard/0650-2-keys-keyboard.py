86a031fa7281bc62e7a74eeefe603369d32f39e4
Debashish Sahoo <138712313+Debashishsaho@users.noreply.github.com>
2024-08-19 08:43:49 +0530

    Time: 391 ms (17.54%), Space: 16.6 MB (61.96%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,27 +1,12 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        def isprime(n):
-            val=0
-            for i in range(2, (n//2)+1):
-                if n % i == 0:
-                    val=i
-            if val==0:
-                return True
-            else:
-                return val
         if n==1:
             return 0
-        elif isprime(n)==True:
-            return n
         dp=[0]*(n+1)
         dp[1]=0
         for i in range(2,n+1):
-            if i<=5:
-                dp[i]=i
-            else:
-                if isprime(i)==True:
-                    dp[i]=i
-                else:
-                    ind=isprime(i)
-                    dp[i]= dp[ind]+(i//ind)
-        return dp[n]
+            dp[i]=i
+            for j in range(2,(n//2)+1):
+                if i%j==0:
+                    dp[i]=min(dp[i],dp[j]+i//j,dp[i//j]+j)
+        return dp[-1]
