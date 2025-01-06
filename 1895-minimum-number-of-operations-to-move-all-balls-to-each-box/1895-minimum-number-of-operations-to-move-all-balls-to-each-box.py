c8083177a0aee443e05b3fc1807077444eeee034
Debashish Sahoo <138712313+Debashishsaho@users.noreply.github.com>
2025-01-06 16:11:50 +0530

    Time: 11 ms (68.21%), Space: 18.1 MB (16.05%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,10 +1,29 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        ans=[0]*len(boxes)
-        for i in range(len(boxes)):
-            for j in range(len(boxes)):
-                if i==j:
-                    continue
-                elif boxes[j]=='1':
-                    ans[i] += abs(i-j)
+        n=len(boxes)
+        prefix_sum=[0]*n
+        sufix_sum=[0]*n
+        ans=[0]*n
+        count=0
+        for i in range(n):
+            if count==0:
+                prefix_sum[i]=0
+                if boxes[i]=="1":
+                    count+=1
+                continue
+            prefix_sum[i] += prefix_sum[i-1] + count
+            if boxes[i]=="1":
+                count+=1
+        count=0
+        for i in range(n-1,-1,-1):
+            if count==0:
+                sufix_sum[i]=0
+                ans[i]=prefix_sum[i]+sufix_sum[i]
+                if boxes[i]=="1":
+                    count+=1
+                continue
+            sufix_sum[i] += sufix_sum[i+1] + count
+            if boxes[i]=="1":
+                count+=1
+            ans[i]=prefix_sum[i]+sufix_sum[i]
         return ans
